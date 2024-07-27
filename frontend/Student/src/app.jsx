8d65205fbc9737f8ca196ce0592c7105c2b973be
Prapti Saxena <praptisaxena685@gmail.com>
2024-07-28 02:01:58 +0530

    uhbh

--- frontend/Student/src/app.jsx
+++ frontend/Student/src/app.jsx
@@ -1,8 +1,6 @@
 import React, { useContext, useEffect } from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
 import Register from "./components/register.jsx";
 
 
@@ -10,27 +8,6 @@ import axios from "axios";
 import { Context } from "./main";
 import Login from "./components/StudentLogin.jsx";
 const App = () => {
-  const { isAuthenticated, setIsAuthenticated, setUser } =
-    useContext(Context);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:4000/api/v1/user/patient/me",
-          {
-            withCredentials: true,
-          }
-        );
-        setIsAuthenticated(true);
-        setUser(response.data.user);
-      } catch (error) {
-        setIsAuthenticated(false);
-        setUser({});
-      }
-    };
-    fetchUser();
-  }, [isAuthenticated]);
 
   return (
     <>
@@ -40,7 +17,7 @@ const App = () => {
           <Route path="/login" element={<StudentLogin />} />
           {/* <Route path="/appointment" element={<Appointment />} />
           <Route path="/about" element={<AboutUs />} /> */}
-          <Route path="/login/register" element={<Register />} />
+          <Route path="/register" element={<Register />} />
           {/* <Route path="/login" element={<Login />} /> */}
         </Routes>
         <Footer />
