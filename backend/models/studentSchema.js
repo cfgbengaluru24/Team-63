b43ad01c8fc308ec5750de6712b8567acd1d4efa
Sehaj-pec <deepsehaj249@gmail.com>
2024-07-27 22:32:49 +0530

    updated student schema

--- backend/models/studentSchema.js
+++ backend/models/studentSchema.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
+import bcrypt from "bcrypt"
 
-
-
-const studentSchema=new moongose.Schema({
+const studentSchema=new mongoose.Schema({
     name:{
         type:String,
         required: [true,"Name is Required"]
@@ -23,6 +22,12 @@ const studentSchema=new moongose.Schema({
         type:Boolean
     }
 });
+
+studentSchema.pre("save" , async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password , 10)
+    next()
+})
 
 const Student = mongoose.model('Student', studentSchema);
 
